7c68bd7c0562a82a8db61c5ba0fc02646b372beb
郭源 <gy378249555@gmail.com>
2019-08-09 11:38:42 +0800

    [Update] mst 0.2.0

--- Formula/mst.rb
+++ Formula/mst.rb
@@ -2,14 +2,14 @@ class Mst < Formula
   desc 'Tool to analyze iOS app size'
   homepage 'https://github.com/guoyuan94/mst'
   url 'https://github.com/guoyuan94/mst.git',
-      tag: '0.1.0',
-      revision: '2e2c2a06d849c742c28ac24b512a8c1260688749'
+      tag: '0.2.0',
+      revision: 'dcc798ad3f0566b969fe4fbdf06d1f2456e78aa0'
   head 'https://github.com/guoyuan94/mst.git'
 
   bottle do
-    root_url 'https://github.com/guoyuan94/mst/releases/download/0.1.0'
+    root_url 'https://github.com/guoyuan94/mst/releases/download/0.2.0'
     cellar :any_skip_relocation
-    sha256 'af5ae24bd59b4e76f96b35caa857b6455a5c50fe4cace546c45648bbf3887e05' => :mojave
+    sha256 'a3bd0aaff031b5f6c3b68e08cd9d9a108ed2c17dcb2f2c477f2707cc2a1aa88e' => :mojave
   end
 
   depends_on xcode: ['10.2', :build]
